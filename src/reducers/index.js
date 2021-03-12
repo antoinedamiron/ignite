ae8c5c0190e0d5e84301c1363fcb9f0c91d7ebd4
antoine <antoine@antoine.home>
2021-03-12 16:05:24 +0100

    Fetch games details

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 //All the different reducers
 import gamesReducer from "./gamesReducer";
+import detailReducer from "./detailReducer";
 
 const rootReducer = combineReducers({
   games: gamesReducer,
+  detail: detailReducer,
 });
 
 export default rootReducer;
