ce1af299f9c48bbd65f8a001e4f1c68e6156ecf1
antoine <antoine@antoine.home>
2021-03-13 14:52:59 +0100

    Nav searching

--- src/api.js
+++ src/api.js
@@ -44,3 +44,6 @@ export const gameDetailsURL = (game_id) => `${base_url}games/${game_id}`;
 //Game ScreenShots
 export const gameScreenshotsURL = (game_id) =>
   `${base_url}games/${game_id}/screenshots`;
+//Searched Game
+export const searchGameURL = (game_name) =>
+  `${base_url}games?search=${game_name}&page_size=9`;
